6e10af0f4d4d0a72e2dce085bc343004b2398d83
SirCactus <cactu248@hotmail.co.uk>
2016-03-19 14:27:14 +0000

    change is good because balls

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		this.transform.Translate (.25f,0f,0f);
+	}
+}
