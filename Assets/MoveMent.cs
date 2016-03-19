7599cac2db6db212dfb347c446e09aaf378f4c9b
SirCactus <cactu248@hotmail.co.uk>
2016-03-19 13:37:37 +0000

    can

---
+++ Assets/MoveMent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		if (Input.GetKey("w")){
+			this.transform.Translate (0f,1f,0f);
+		}
+		else if (Input.GetKey("s")){
+			this.transform.Translate (0f,-1f,0f);
+		}
+		else if (Input.GetKey("a")){
+			this.transform.Translate (-1f,0f,0f);
+		}
+		else if (Input.GetKey("d")){
+			this.transform.Translate (1f,0f,0f);
+		}
+	}
+}
