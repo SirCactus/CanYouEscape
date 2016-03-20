fb332215360990c1d902311b2bb6208fa1365501
SirCactus <cactu248@hotmail.co.uk>
2016-03-20 10:08:35 +0000

    added manual jump, might be an idea to use in-engine physics

--- Assets/MoveMent.cs
+++ Assets/MoveMent.cs
@@ -3,29 +3,35 @@ using System.Collections;
 
 public class MoveMent : MonoBehaviour {
 
+	float startHeight;
+	void Start(){
+		startHeight = this.transform.position.y;
+	}
+
 	void FixedUpdate(){
 		this.transform.Translate (.25f,0f,0f);
-		if (Input.GetKey("w")&& this.yFromCamera()<2.0f){
-			this.transform.Translate (0f,.2f,0f);
+		if (this.transform.position.y > startHeight) {
+			this.transform.Translate (0f,-.1f,0f);
 		}
-		else if (Input.GetKey("s")&& this.yFromCamera()>-2.0f){
+		if (Input.GetKey("w")&& this.yFromCamera()<5f&&startHeight==this.transform.position.y){
+			this.transform.Translate (0f,2f,0f);
+		}
+		else if (Input.GetKey("s")&& this.yFromCamera()>-3f){
 			this.transform.Translate (0f,-.2f,0f);
 		}
-		if (Input.GetKey("a")&& this.xFromCamera()<2.0f){
-			this.transform.Translate (-.2f,0f,0f);
+		if (Input.GetKey ("d") && this.xFromCamera () < 7.5f) {
+			this.transform.Translate (.2f, 0f, 0f);
 		}
-		else if (Input.GetKey("d")&& this.xFromCamera()>-2.0f){
-			this.transform.Translate (.2f,0f,0f);
+		else if (Input.GetKey("a")&& this.xFromCamera()>-7.5f){
+			this.transform.Translate (-.2f,0f,0f);
 		}
 	}
 
-	float xFromCamera(){
-		Debug.Log (this.transform.position.x - Camera.main.transform.position.x);
+	private float xFromCamera(){
 		return ((this.transform.position.x)-(Camera.main.transform.position.x));
 	}
 
-	float yFromCamera(){
-		Debug.Log ((this.transform.position.y)-(Camera.main.transform.position.y));
+	private float yFromCamera(){
 		return ((this.transform.position.y)-(Camera.main.transform.position.y));
 	}
 }
