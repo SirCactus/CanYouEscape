f676582aa38c0942745447964c09f5311350ff9b
SirCactus <cactu248@hotmail.co.uk>
2016-03-20 16:26:28 +0000

    Signed-off-by: SirCactus <cactu248@hotmail.co.uk>

--- Assets/CameraMove.cs
+++ Assets/CameraMove.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class CameraMove : MonoBehaviour {
 
+	//public Transform follow;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
-		//this.transform.Translate (.25f,0f,0f);
+//		this.position = new Vector3(follow.position.x, this.transform.position.y,this.transform.position.z);
+		this.transform.Translate (.25f,0f,0f);
 	}
 }
--- Assets/MoveMent.cs
+++ Assets/MoveMent.cs
@@ -8,26 +8,23 @@ public class MoveMent : MonoBehaviour {
 		startHeight = this.transform.position.y;
 	}
 
+//	5f&&startHeight==this.transform.position.
+
 	void FixedUpdate(){
-		this.transform.Translate (.25f,0f,0f);
-		if (this.transform.position.y > startHeight) {
-			this.transform.Translate (0f,-.1f,0f);
+		this.transform.Translate (.25f, 0f, 0f);
+//		if (this.transform.position.y > startHeight) {
+//			this.transform.Translate (0f,-.1f,0f);
+//		}
+		if (Input.GetKey ("w")) {
+			this.transform.Translate (0f, .5f, 0f);
 		}
-		if (Input.GetKey("w")&& this.yFromCamera()<5f&&startHeight==this.transform.position.y){
-			//this.transform.Translate (0f,2f,0f);
-
-		}
-		else if (Input.GetKey("s")&& this.yFromCamera()>-3f){
-			this.transform.Translate (0f,-.2f,0f);
-		}
-		if (Input.GetKey ("d") && this.xFromCamera () < 7.5f) {
-			this.transform.Translate (.2f, 0f, 0f);
-		}
-		else if (Input.GetKey("a")&& this.xFromCamera()>-7.5f){
-			this.transform.Translate (-.2f,0f,0f);
-		}
+		//	if (Input.GetKey ("d") && this.xFromCamera () < 7.5f) {
+		//		this.transform.Translate (.2f, 0f, 0f);
+		//	}
+		//	else if (Input.GetKey("a")&& this.xFromCamera()>-7.5f){
+		//		this.transform.Translate (-.2f,0f,0f);
+		//	}
 	}
-
 	private float xFromCamera(){
 		return ((this.transform.position.x)-(Camera.main.transform.position.x));
 	}
